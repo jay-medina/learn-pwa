de056626404188b53fef894aa18f78394d032aad
Jose Medina <josemedina921@gmail.com>
2017-05-29 21:51:15 -0700

    install and activate the service worker

--- service-worker/js/other.js
+++ service-worker/js/other.js
@@ -13,21 +13,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-(function() {
-  'use strict';
-
-  // TODO - 3.1: Add install and activate event listeners
-  self.addEventListener('install', (event) => {
-    console.log('service worker installing...');
-    // TODO 3.4: Skip waiting...
-  });
-
-  self.addEventListener('activate', (event) => {
-    console.log('service worker activating...');
-  });
-
-  // TODO - 3.3: I'm a new service worka
-
-  // TODO - 4: Add fetch listener
-
-})();
